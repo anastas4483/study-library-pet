37b4f4c4bf5acaae637a14a768ed58709fdf441a
Anastasia Sorokina <57874418+anastas4483@users.noreply.github.com>
2024-07-04 01:20:40 +0300

    interact with pet

--- index.ts
+++ index.ts
@@ -3,3 +3,10 @@ import { AnimalColor } from "./types/animal-color";
 import { AnimalType } from "./types/animal-type";
 
 export const cat = new Pet('barsik', AnimalType.Cat, AnimalColor.Black, 12);
+console.log('isHungry', cat.isHungry);
+cat.feeding();
+console.log('isHungry', cat.isHungry);
+console.log('_______');
+console.log('age', cat.age);
+cat.happyBirthdayMyPet();
+console.log('age', cat.age);
